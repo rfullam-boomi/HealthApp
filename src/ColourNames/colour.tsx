5403ec52ef44b2793a107ee0782b7972d9206136
markwattsboomi <markwatts@boomi.com>
2022-05-18 11:23:32 +0100

    ColourNames is now forced to 50% matches

--- src/ColourNames/colour.tsx
+++ src/ColourNames/colour.tsx
@@ -15,11 +15,25 @@ export default class ColourName extends React.Component<any,any> {
     correct: boolean = false;
     
     randomise() {
+        let match: number = Math.floor(Math.random()*2);
         let labelPos: number = Math.floor(Math.random()*this.colours.length);
         this.label = this.colours[labelPos].name;
-        let colorPos: number = Math.floor(Math.random()*this.colours.length);
-        this.colour = this.colours[colorPos].color;
-        this.correct = labelPos===colorPos;
+        if(match===1) {
+            this.colour = this.colours[labelPos].color;
+            this.correct=true;
+        }
+        else {
+            let colorPos: number = Math.floor(Math.random()*this.colours.length);
+            while(colorPos === labelPos) {
+                colorPos = Math.floor(Math.random()*this.colours.length);
+            }
+            this.colour = this.colours[colorPos].color;
+            this.correct = labelPos===colorPos;
+            this.forceUpdate();
+        }
+        //let colorPos: number = Math.floor(Math.random()*this.colours.length);
+        //this.colour = this.colours[colorPos].color;
+        //this.correct = labelPos===colorPos;
         this.forceUpdate();
     }
 
